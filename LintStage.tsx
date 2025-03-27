fa2b1a3c2be043cc427fde9216fd99ae3642a72c
bsj_山口流石 <110379934+bsj-s-yamaguchi@users.noreply.github.com>
2025-03-27 15:12:01 +0900

    Initial commit

---
+++ LintStage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const LintStage = () => {
+    return (
+        <div>
+            <p>lint-stageのテストだよ</p>
+            <p>lint-stageのテストだよ</p>
+            <p>lint-stageのテストだよ</p>
+            <p>lint-stageのテストだよ</p>
+        </div>
+    );
+};
+
+export default LintStage;
